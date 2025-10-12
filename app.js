704bd5546b53aea2f7782bd66e739b290abec1df
Umwizer <ruthumwizerwa@gmail.com>
2025-10-12 11:55:20 +0200

    creating env development and production

---
+++ app.js
@@ -0,0 +1,15 @@
+import express from "express";
+import { PORT } from "./config/env.js";
+const app = express();
+// const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the Subscription Tracker API!");
+});
+
+app.listen(PORT, () => {
+  console.log(
+    `Subscription Tracker Api is running on http://localhost:${PORT}`
+  );
+});
+export default app;
---
+++ config/env.js
@@ -0,0 +1,9 @@
+import { config } from "dotenv";
+
+// Load environment variables from .env file based on NODE_ENV
+config({
+  path: `.env.${process.env.NODE_ENV || "development"}.local`,
+});
+
+// Destructure variables from process.env
+export const { PORT, NODE_ENV } = process.env;
