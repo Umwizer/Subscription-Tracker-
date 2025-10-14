1f49c0267f41a816d18d47c5931014dab60319ad
Umwizer <ruthumwizerwa@gmail.com>
2025-10-14 10:04:55 +0200

    creating database

--- app.js
+++ app.js
@@ -4,19 +4,32 @@ import { PORT } from "./config/env.js";
 import userRoutes from "./routes/user.routes.js";
 import authRoutes from "./routes/auth.routes.js";
 import SubscriptionRoutes from "./routes/subscription.routes.js";
+import connectToDatabase from "./databases/mongodb.js";
+
 const app = express();
-// const PORT = 3000;
 
 app.use("/api/v1/auth", authRoutes);
-app.use("api/v1/user", userRoutes);
+app.use("/api/v1/user", userRoutes);
 app.use("/api/v1/sub", SubscriptionRoutes);
+
 app.get("/", (req, res) => {
   res.send("Welcome to the Subscription Tracker API!");
 });
 
-app.listen(PORT, () => {
-  console.log(
-    `Subscription Tracker Api is running on http://localhost:${PORT}`
-  );
-});
+// Connect to MongoDB before starting the server
+const startServer = async () => {
+  try {
+    await connectToDatabase(); // connect first
+    app.listen(PORT, () => {
+      console.log(
+        `Subscription Tracker API is running on http://localhost:${PORT}`
+      );
+    });
+  } catch (err) {
+    console.error("Failed to start server:", err);
+  }
+};
+
+startServer();
+
 export default app;
